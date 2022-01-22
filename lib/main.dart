fbf30bf58548426f9488e2752843952a47c8c1d7
Jady Sobjak de Mello Godoi <jady.s.m@gmail.com>
2022-01-21 21:12:29 -0300

    config fonts and colors

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        // primarySwatch: Colors.pink,
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        colorScheme: const ColorScheme(
+          primary: Colors.pink,
+          primaryVariant: Colors.red,
+          secondary: Colors.amber,
+          secondaryVariant: Colors.brown,
+          surface: Colors.grey,
+          background: Colors.black,
+          error: Colors.red,
+          onPrimary: Colors.white,
+          onSecondary: Colors.black,
+          onSurface: Colors.green,
+          onBackground: Colors.white10,
+          onError: Colors.redAccent,
+          brightness: Brightness.light,
+        ),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: const TextStyle(
+                fontFamily: 'RobotoCondensed',
+                fontSize: 20,
+              ),
+            ),
       ),
       home: const CategoriesScreem(),
     );
