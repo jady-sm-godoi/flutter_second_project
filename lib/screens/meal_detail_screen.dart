6da8447431b4a23267dcc80e1b08d21858639882
Jady Sobjak de Mello Godoi <jady.s.m@gmail.com>
2022-01-26 21:20:13 -0300

    ingredients card list

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,17 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:udemy_recipe/models/meal.dart';
 
 class MealDetailScreen extends StatelessWidget {
   const MealDetailScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final meal = ModalRoute.of(context)!.settings.arguments as Meal;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Titulo da receita'),
-      ),
-      body: Center(
-        child: Text('receita em detalhes'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(meal.title!),
+        ),
+        body: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                meal.imageUrl!,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'Ingredientes',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            ),
+            Container(
+              width: 300,
+              height: 200,
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10)),
+              child: ListView.builder(
+                  itemCount: meal.ingredients!.length,
+                  itemBuilder: (item, index) {
+                    return Card(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 5,
+                          horizontal: 10,
+                        ),
+                        child: Text(meal.ingredients![index]),
+                      ),
+                      color: Theme.of(context).colorScheme.secondary,
+                    );
+                  }),
+            )
+          ],
+        ));
   }
 }
