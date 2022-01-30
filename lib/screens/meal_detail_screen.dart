34c46ad8967cc0fb14c5c67bf4732b38d357c423
Jady Sobjak de Mello Godoi <jady.s.m@gmail.com>
2022-01-30 11:59:14 -0300

    ingredients card and refactor

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -4,6 +4,29 @@ import 'package:udemy_recipe/models/meal.dart';
 class MealDetailScreen extends StatelessWidget {
   const MealDetailScreen({Key? key}) : super(key: key);
 
+  Widget _createSectionTitle(BuildContext context, String title) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.headline6,
+      ),
+    );
+  }
+
+  Widget _createSectionContainer(child) {
+    return Container(
+        width: 350,
+        height: 200,
+        padding: const EdgeInsets.all(10),
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(10)),
+        child: child);
+  }
+
   @override
   Widget build(BuildContext context) {
     final meal = ModalRoute.of(context)!.settings.arguments as Meal;
@@ -12,48 +35,58 @@ class MealDetailScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text(meal.title!),
         ),
-        body: Column(
-          children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Image.network(
-                meal.imageUrl!,
-                fit: BoxFit.cover,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 300,
+                width: double.infinity,
+                child: Image.network(
+                  meal.imageUrl!,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10),
-              child: Text(
-                'Ingredientes',
-                style: Theme.of(context).textTheme.headline6,
-              ),
-            ),
-            Container(
-              width: 300,
-              height: 200,
-              padding: EdgeInsets.all(10),
-              margin: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(10)),
-              child: ListView.builder(
-                  itemCount: meal.ingredients!.length,
-                  itemBuilder: (item, index) {
-                    return Card(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 5,
-                          horizontal: 10,
+              _createSectionTitle(context, 'Ingredientes'),
+              _createSectionContainer(
+                ListView.builder(
+                    itemCount: meal.ingredients!.length,
+                    itemBuilder: (item, index) {
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 5,
+                            horizontal: 10,
+                          ),
+                          child: Text(meal.ingredients![index]),
                         ),
-                        child: Text(meal.ingredients![index]),
+                        color: Theme.of(context).colorScheme.secondary,
+                      );
+                    }),
+              ),
+              _createSectionTitle(context, 'Passos'),
+              _createSectionContainer(ListView.builder(
+                itemCount: meal.steps?.length,
+                itemBuilder: (ctx, index) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(
+                          child: Text(
+                            '${index + 1}',
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                          backgroundColor:
+                              Theme.of(context).colorScheme.primary,
+                        ),
+                        title: Text(meal.steps![index]),
                       ),
-                      color: Theme.of(context).colorScheme.secondary,
-                    );
-                  }),
-            )
-          ],
+                      const Divider()
+                    ],
+                  );
+                },
+              ))
+            ],
+          ),
         ));
   }
 }
